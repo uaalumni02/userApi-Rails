77dbdf9f98b6837367e4f291eeaffc00fcd6a042
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-10-18 20:00:27 -0400

    add fact controller

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -1,2 +1,50 @@
 class Api::V1::FactsController < ApplicationController
+    before_action :find_fact, only: [:show, :update, :destroy]
+
+def index
+    @facts = Fact.all
+    render json: @facts
+end
+
+def show
+    render json: @fact
+end
+
+def create
+    @fact = Fact.new(fact_params)
+    if @fact.save
+        render json: @fact
+    else 
+        render error(error: 'unable to create fact'), status: 400
+    end
+end
+
+def update
+    if @fact
+        @fact.update(fact_params)
+        render json: {message: 'fact successfully updated'}, status: 200
+    else
+        render json: {error: 'unable to update fact'}, status: 400
+    end 
+end 
+
+def destroy
+    if @fact
+        @fact.destroy
+        render json: {message: 'fact deleted'}, status: 200
+    else
+        render json: {error: 'unable to delete'}, status: 400
+    end
+end
+
+private
+
+def fact_params
+    params.require(:fact).permit(:fact, :likes, :user_id)
+end
+
+def find_fact
+    @fact = Fact.find(params[:id])
+end
+
 end
